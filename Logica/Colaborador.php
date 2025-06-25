f53ed22728ff6b4c288208b89868c5f186818931
Sebas <sebastianmahechat@gmail.com>
2025-06-24 23:41:30 -0500

    Subida inicial del proyecto EcoGestor

--- Logica/Colaborador.php
+++ Logica/Colaborador.php
@@ -1,5 +1,7 @@
 <?php
-require_once 'Persona.php';
+require_once("persistencia/Conexion.php");
+require_once("persistencia/ColaboradorDAO.php");
+require_once("logica/Persona.php");
 
 class Colaborador extends Persona {
     private $direccionOficina;
@@ -13,28 +15,61 @@ class Colaborador extends Persona {
         $this->domicilio = $domicilio;
     }
 
-    public function getDireccionOficina() {
-        return $this->direccionOficina;
+    // Getters
+    public function getDireccionOficina() { return $this->direccionOficina; }
+    public function getTelefono() { return $this->telefono; }
+    public function getDomicilio() { return $this->domicilio; }
+
+    // Setters
+    public function setDireccionOficina($direccionOficina) { $this->direccionOficina = $direccionOficina; }
+    public function setTelefono($telefono) { $this->telefono = $telefono; }
+    public function setDomicilio($domicilio) { $this->domicilio = $domicilio; }
+
+    // Métodos Lógicos
+
+    public function autenticar() {
+        $conexion = new Conexion();
+        $colaboradorDAO = new ColaboradorDAO(0, "", $this->correo, $this->clave);
+        $conexion->abrir();
+        $conexion->ejecutar($colaboradorDAO->autenticar());
+        if ($conexion->filas() == 1) {
+            $this->id = $conexion->registro()[0];
+            $conexion->cerrar();
+            return true;
+        } else {
+            $conexion->cerrar();
+            return false;
+        }
     }
 
-    public function getTelefono() {
-        return $this->telefono;
+    public function consultar() {
+        $conexion = new Conexion();
+        $colaboradorDAO = new ColaboradorDAO($this->id);
+        $conexion->abrir();
+        $conexion->ejecutar($colaboradorDAO->consultar());
+        $datos = $conexion->registro();
+        $this->nombre = $datos[0];
+        $this->correo = $datos[1];
+        $this->direccionOficina = $datos[2];
+        $this->telefono = $datos[3];
+        $this->domicilio = $datos[4];
+        $conexion->cerrar();
     }
 
-    public function getDomicilio() {
-        return $this->domicilio;
-    }
+    public static function buscar($filtro) {
+        $conexion = new Conexion();
+        $colaboradorDAO = new ColaboradorDAO();
+        $conexion->abrir();
+        $conexion->ejecutar($colaboradorDAO->buscar($filtro));
+        $colaboradores = array();
 
-    public function setDireccionOficina($direccionOficina) {
-        $this->direccionOficina = $direccionOficina;
-    }
+        while ($registro = $conexion->registro()) {
+            $colaborador = new Colaborador($registro[0], $registro[1], "", $registro[2]);
+            array_push($colaboradores, $colaborador);
+        }
 
-    public function setTelefono($telefono) {
-        $this->telefono = $telefono;
-    }
-
-    public function setDomicilio($domicilio) {
-        $this->domicilio = $domicilio;
+        $conexion->cerrar();
+        return $colaboradores;
     }
 }
 ?>
--- Logica/Publicacion.php
+++ Logica/Publicacion.php
@@ -1,6 +1,9 @@
 <?php
+require_once("persistencia/Conexion.php");
+require_once("persistencia/PublicacionDAO.php");
+
 class Publicacion {
-private $id;
+    private $id;
     private $colaboradorId;
     private $titulo;
     private $descripcion;
@@ -18,6 +21,7 @@ private $id;
         $this->tipo = $tipo;
     }
 
+    // Getters
     public function getId() { return $this->id; }
     public function getColaboradorId() { return $this->colaboradorId; }
     public function getTitulo() { return $this->titulo; }
@@ -26,6 +30,7 @@ private $id;
     public function getFechaPublicacion() { return $this->fechaPublicacion; }
     public function getTipo() { return $this->tipo; }
 
+    // Setters
     public function setId($id) { $this->id = $id; }
     public function setColaboradorId($colaboradorId) { $this->colaboradorId = $colaboradorId; }
     public function setTitulo($titulo) { $this->titulo = $titulo; }
@@ -33,6 +38,40 @@ private $id;
     public function setEnlace($enlace) { $this->enlace = $enlace; }
     public function setFechaPublicacion($fechaPublicacion) { $this->fechaPublicacion = $fechaPublicacion; }
     public function setTipo($tipo) { $this->tipo = $tipo; }
-};
 
+    // 🔍 Consultar una publicación específica
+    public function consultar() {
+        $conexion = new Conexion();
+        $publicacionDAO = new PublicacionDAO($this->id);
+        $conexion->abrir();
+        $conexion->ejecutar($publicacionDAO->consultar());
+        if ($conexion->filas() > 0) {
+            $datos = $conexion->registro();
+            $this->colaboradorId = $datos[1];
+            $this->titulo = $datos[2];
+            $this->descripcion = $datos[3];
+            $this->enlace = $datos[4];
+            $this->fechaPublicacion = $datos[5];
+            $this->tipo = $datos[6];
+        }
+        $conexion->cerrar();
+    }
+
+    // 🔍 Buscar publicaciones por filtro
+    public static function buscar($filtro) {
+        $conexion = new Conexion();
+        $publicacionDAO = new PublicacionDAO();
+        $conexion->abrir();
+        $conexion->ejecutar($publicacionDAO->buscar($filtro));
+        $publicaciones = array();
+
+        while ($registro = $conexion->registro()) {
+            $publicacion = new Publicacion($registro[0], "", $registro[1], "", "", "", $registro[2]);
+            array_push($publicaciones, $publicacion);
+        }
+
+        $conexion->cerrar();
+        return $publicaciones;
+    }
+}
 ?>
